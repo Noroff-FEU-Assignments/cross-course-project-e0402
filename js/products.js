d33c83034a0654f1d2c05777ea1775f0748e3e7e
e0402 <100483961+e0402@users.noreply.github.com>
2022-10-26 18:34:55 +0200

    test

--- js/products.js
+++ js/products.js
@@ -3,7 +3,7 @@ const url = "https://schoolproject.tech/flowerpower/wp-json/wc/store/products";
 async function fetchProducts() {
   try {
     const response = await fetch(url);
-    const fetchResults = await response.json;
+    const fetchResults = await response.json();
     console.log(fetchResults);
   } catch (error) {
     console.log(error);
