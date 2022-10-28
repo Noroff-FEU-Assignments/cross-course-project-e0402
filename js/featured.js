bf980ed9224991b14426b74fcadbd96a89456012
e0402 <100483961+e0402@users.noreply.github.com>
2022-10-28 15:37:49 +0200

    Update featured.js

--- js/featured.js
+++ js/featured.js
@@ -1,6 +1,6 @@
 const url =
   "https://schoolproject.tech/flowerpower/wp-json/wc/v3/products?consumer_key=ck_6c90a14e6f8635c6ec35e78ebd8f73b23a0647cc&consumer_secret=cs_f1de37e9ba6d73daacf712e2536c593462ddce89&featured=true";
-const productContainer = document.querySelector(".featured_section");
+const productContainer = document.querySelector(".products_section_three");
 
 async function fetchProducts() {
   try {
@@ -14,8 +14,8 @@ async function fetchProducts() {
 
 fetchProducts();
 
-function createHTML(featured_section) {
-  featured_section.forEach((product) => {
+function createHTML(products_section_three) {
+  products_section_three.forEach((product) => {
     productContainer.innerHTML += `<div class="product-container">
         <div class="product featured-product_background">
             <img src="${product.images[0].src}" class="product_image" alt="${product.name}">
